0c94b6c8a66954e080334f10bf83c299e8d9b96d
MKS-VW <viktor.wiens@mks360.de>
2022-09-14 09:08:53 +0200

    25. Menü 'Gerichte bearbeiten' angelegt

--- src/Controller/GerichtController.php
+++ src/Controller/GerichtController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\Gericht;
+use App\Repository\GerichtRepository;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/gericht', name: 'app_gericht.')]
 class GerichtController extends AbstractController
 {
-    #[Route('/gericht', name: 'app_gericht')]
-    public function index(): Response
-    {
+    #[Route('/', name: 'bearbeiten')]
+    public function index(GerichtRepository $gr): Response{
+        $gerichte = $gr->findAll();
+        
         return $this->render('gericht/index.html.twig', [
-            'controller_name' => 'GerichtController',
+            'gerichte' => $gerichte
         ]);
     }
+    
+    #[Route('/anlegen', name: 'anlegen')]
+    public function anlegen(ManagerRegistry $doctrine): Response{
+        $gericht = new Gericht();
+        $gericht->setName("Pizza");
+
+        //EntityManager
+        $em = $doctrine->getManager();
+        $em->persist($gericht);
+        $em->flush();
+
+        //Response
+        return new Response("Gericht wurde angelegt");
+    }
 }
+
